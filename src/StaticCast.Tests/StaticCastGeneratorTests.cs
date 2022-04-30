1c61d1fbd44bc47ee9c0b05e169d6f920ea81058
Jason Bock <jason.r.bock@outlook.com>
2022-04-30 14:26:42 -0500

    #9 added a couple of tests

--- src/StaticCast.Tests/StaticCastGeneratorTests.cs
+++ src/StaticCast.Tests/StaticCastGeneratorTests.cs
@@ -289,7 +289,213 @@ public static class StaticCastGeneratorTests
 
 	// TODO: Test where two interfaces have the same signature.
 	// Should only generate one
+	[Test]
+	public static async Task GenerateWhereTwoMethodsFromTwoInterfacesHaveSameSignatureAsync()
+	{
+		var code =
+			"""
+			public interface IWork
+			{
+				static abstract void Work(string data);
+			}
 
-	// TODO: Test where interface has multiple members,
-	// Should generate all even if only one is being "used".
+			public interface IExercise
+			{
+				static abstract void Work(string data);
+			}
+
+			public static class Test
+			{
+				public static void CallIWork() =>
+					StaticCast<object, IWork>.Void.Work("data");
+
+				public static void CallIExercise() =>
+					StaticCast<object, IExercise>.Void.Work("data");
+			}
+			""";
+
+		var generatedCode =
+			"""
+			using System;
+			using System.Reflection;
+			
+			#nullable enable
+			public static class StaticCast<T, TAs>
+			{
+				private static void Verify()
+				{
+					var tType = typeof(T);
+					
+					if (tType.IsInterface)
+					{
+						throw new NotSupportedException($"The T type, {tType.FullName}, is an interface.");
+					}
+					else if (tType.IsAbstract)
+					{
+						throw new NotSupportedException($"The T type, {tType.FullName}, is abstract.");
+					}
+					
+					var asType = typeof(TAs);
+					
+					if (!asType.IsInterface)
+					{
+						throw new NotSupportedException($"The TAs type, {asType.FullName}, is not an interface.");
+					}
+				}
+				
+				private static MethodInfo GetTargetMethod(MethodInfo interfaceMethod)
+				{
+					var interfaceMap = typeof(T).GetInterfaceMap(typeof(TAs));
+					
+					MethodInfo? targetMethod = null;
+					
+					for (var i = 0; i < interfaceMap.InterfaceMethods.Length; i++)
+					{
+						if (interfaceMap.InterfaceMethods[i] == interfaceMethod)
+						{
+							targetMethod = interfaceMap.TargetMethods[i]!;
+						}
+					}
+					
+					if (targetMethod is null)
+					{
+						throw new NotSupportedException(
+							$"{typeof(TAs).FullName} does not have a mapping for {interfaceMethod.Name} on type {typeof(T).FullName}");
+					}
+					
+					return targetMethod!;
+				}
+				
+				public static class Void
+				{
+					public static void Work(string data)
+					{
+						Verify();
+						
+						if (typeof(T).IsAssignableTo(typeof(TAs)))
+						{
+							var interfaceMethod = typeof(TAs).GetMethod(
+								"Work", BindingFlags.Public | BindingFlags.Static, new[] { typeof(string) })!;
+							var targetMethod = GetTargetMethod(interfaceMethod);
+							targetMethod.Invoke(null, new object[] { data });
+						}
+						
+					}
+				}
+			}
+			
+			""";
+
+		await TestAssistants.RunAsync<StaticCastGenerator>(code,
+			 new[] { (typeof(StaticCastGenerator), "StaticCast.g.cs", generatedCode) },
+			 Enumerable.Empty<DiagnosticResult>()).ConfigureAwait(false);
+	}
+
+	[Test]
+	public static async Task GenerateWhereInterfaceHasMultipleMembersAsync()
+	{
+		var code =
+			"""
+			public interface IWork
+			{
+				static abstract void Work(string data);
+				static abstract void Rest();
+			}
+
+			public static class Test
+			{
+				public static void CallIWork() =>
+					StaticCast<object, IWork>.Void.Work("data");
+			}
+			""";
+
+		var generatedCode =
+			"""
+			using System;
+			using System.Reflection;
+			
+			#nullable enable
+			public static class StaticCast<T, TAs>
+			{
+				private static void Verify()
+				{
+					var tType = typeof(T);
+					
+					if (tType.IsInterface)
+					{
+						throw new NotSupportedException($"The T type, {tType.FullName}, is an interface.");
+					}
+					else if (tType.IsAbstract)
+					{
+						throw new NotSupportedException($"The T type, {tType.FullName}, is abstract.");
+					}
+					
+					var asType = typeof(TAs);
+					
+					if (!asType.IsInterface)
+					{
+						throw new NotSupportedException($"The TAs type, {asType.FullName}, is not an interface.");
+					}
+				}
+				
+				private static MethodInfo GetTargetMethod(MethodInfo interfaceMethod)
+				{
+					var interfaceMap = typeof(T).GetInterfaceMap(typeof(TAs));
+					
+					MethodInfo? targetMethod = null;
+					
+					for (var i = 0; i < interfaceMap.InterfaceMethods.Length; i++)
+					{
+						if (interfaceMap.InterfaceMethods[i] == interfaceMethod)
+						{
+							targetMethod = interfaceMap.TargetMethods[i]!;
+						}
+					}
+					
+					if (targetMethod is null)
+					{
+						throw new NotSupportedException(
+							$"{typeof(TAs).FullName} does not have a mapping for {interfaceMethod.Name} on type {typeof(T).FullName}");
+					}
+					
+					return targetMethod!;
+				}
+				
+				public static class Void
+				{
+					public static void Work(string data)
+					{
+						Verify();
+						
+						if (typeof(T).IsAssignableTo(typeof(TAs)))
+						{
+							var interfaceMethod = typeof(TAs).GetMethod(
+								"Work", BindingFlags.Public | BindingFlags.Static, new[] { typeof(string) })!;
+							var targetMethod = GetTargetMethod(interfaceMethod);
+							targetMethod.Invoke(null, new object[] { data });
+						}
+						
+					}
+					public static void Rest()
+					{
+						Verify();
+						
+						if (typeof(T).IsAssignableTo(typeof(TAs)))
+						{
+							var interfaceMethod = typeof(TAs).GetMethod(
+								"Rest", BindingFlags.Public | BindingFlags.Static, Type.EmptyTypes)!;
+							var targetMethod = GetTargetMethod(interfaceMethod);
+							targetMethod.Invoke(null, null);
+						}
+						
+					}
+				}
+			}
+			
+			""";
+
+		await TestAssistants.RunAsync<StaticCastGenerator>(code,
+			 new[] { (typeof(StaticCastGenerator), "StaticCast.g.cs", generatedCode) },
+			 Enumerable.Empty<DiagnosticResult>()).ConfigureAwait(false);
+	}
 }
